f79ac19600f5e252e22693867d18f2d30e9fe363
Juan <juanbedon26@hotmail.com>
2018-07-02 16:03:44 -0500

    Used annotate

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :bigint(8)        not null, primary key
+#  text        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  question_id :bigint(8)
+#
+# Indexes
+#
+#  index_answers_on_question_id  (question_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (question_id => questions.id)
+#
+
 class Answer < ApplicationRecord
 
   belongs_to :question
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :bigint(8)        not null, primary key
+#  commentable_type :string
+#  text             :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  commentable_id   :bigint(8)
+#
+# Indexes
+#
+#  index_comments_on_commentable_type_and_commentable_id  (commentable_type,commentable_id)
+#
+
 class Comment < ApplicationRecord
 
   belongs_to :commentable, polymorphic: true
